7d3a2596a2d3b360da2256fe5a0297efe68aed53
kriss <kriss@xixs.com>
2025-07-13 05:48:54 +0100

    .

--- opusbookshelf.sh
+++ opusbookshelf.sh
@@ -61,7 +61,7 @@ echo "$ODIR"
 mkdir -p "$ODIR"
 echo "$1" ">>into>>" "$ODIR/$BNAM.opus"
 rm -f "./audiobook.opus"
-ffmpeg-bar -i "$1" -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5" -ac 1 -c:a libopus -b:a 24k "./audiobook.opus"
+ffmpeg-bar -i "$1" -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5" -ac 1 -c:a libopus -b:a 16k "./audiobook.opus"
 mv "./audiobook.opus" "$ODIR/$BNAM.opus"
 rm -f "./audiobook.opus"
 
