ff709e29ca05492f0763c0850f5197b4996af23c
kriss <kriss@xixs.com>
2025-07-14 03:58:02 +0100

    .

--- opusbookshelf.sh
+++ opusbookshelf.sh
@@ -77,7 +77,7 @@ fi
 #echo ${EXIF["Artist"]} / ${EXIF["Album"]} / $FNAM
 
 OLDK=`du -k "$1" | cut -f1`
-OLDT=`ffprobe -loglevel error -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "$1"`
+OLDT=`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "$1"`
 
 ODIR="$DIR_OPUS/${EXIF["Artist"]}/${EXIF["Album"]}"
 
